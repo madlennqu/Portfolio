49a6295d7b8a8ff509c7cb295e250a6532a9b0f8
Madiya <109658110+madlennqu@users.noreply.github.com>
2023-02-01 17:01:28 +0800

    Add files via upload

---
+++ a2-1-list.py
@@ -0,0 +1,29 @@
+# you can download at
+# https://goo.gl/WKdcqo
+
+
+colors = ['red', 'blue', 'green']
+
+colors
+
+colors[0]   # zero base indexing
+colors[1]
+colors[2]
+
+colors.append('purple')
+colors
+
+
+colors.remove('green')
+colors
+
+
+colors.extend(['green', 'yellow'])
+
+'green' in colors
+
+colors[3]
+
+type(colors)
+
+help(colors)
