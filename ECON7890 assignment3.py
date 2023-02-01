19d986b8e28c4b5bf1e26ca5bc5711143d3e1880
Madiya <109658110+madlennqu@users.noreply.github.com>
2023-02-01 16:58:01 +0800

    Add files via upload

---
+++ ECON7890 assignment3.py
@@ -0,0 +1,62 @@
+
+dictionary
+
+def sum_of_dict_value(dictionary, n):
+    # Write your code here
+    x=0
+    for k, v in dictionary.items():
+        if k>n:
+            x += value
+    return x
+
+sum_of_dict_value({10:20, 20:30, 30:40},15)
+
+
+def uniq_list_of_value(dictionary):
+    # Write your code here
+    x= []
+    for k, v in dictionary.items():
+        if v not in x:
+            x.append(v)
+    return x
+
+uniq_list_of_value(z)
+
+z={'a':10,'b':20,'c':20, 'd':30}
+
+def uniq_list_of_value(dictionary):
+    # Write your code here
+    x= []
+    for v in dictionary.values():
+        if v not in x:
+            x.append(v)
+            x.sort()
+    return x
+
+uniq_list_of_value(z)
+
+
+def average_of_values(dictionary):
+    # Write your code here
+    x = 0
+    for v in dictionary.values():
+        x+=v
+    return float(x/len(dictionary))
+
+def is_password_valid(password):
+    # Write your code here
+    import re
+    if len(password)<8:
+        if len(password)>16:
+            if re.search("[a-z]", password):
+                if re.search("[A-Z]", password):
+                    if re.search("[0-9]", password):
+                        if re.search("[$#@]", password):
+                            if sum(x.isnumeric() for x in password)>0:
+                                if sum(y.isupper() for y in password)>1:
+                                     if sum(z.islower() for z in password)>1:
+                                        if '$' in password or '&' in password or '@' in password:
+                                            return True
+    return False
+is_password_valid('PassworD1234$')
+#special_chars =  ['$', '&', '!']
